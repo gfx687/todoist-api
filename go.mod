0f718c58e13d236a1784a07bfebb0c77b68100dd
gfx687 <cuzmin.dmitry@gmail.com>
2025-01-24 10:46:50 +0400

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gfx687/todoist-api
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+// TODO: auth
+
+func GetProjects() ([]ProjectSimple, error) {
+	return []ProjectSimple{{Id: "123123", Name: "Inbox"}}, nil
+}
+
+type ProjectSimple struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
